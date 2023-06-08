98977fa67e4ff6bde2de8d30a9f3fc19347af158
Terry-Gumani <terrencegumani0@gmail.com>
2023-06-08 08:00:46 -0700

    Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 
-# main.py
 if __name__ == "__main__":
     """Print the sum, difference, multiple and quotient of 10 and 5."""
+
 from calculator_1 import add, subtract, multiply, divide
 
-a = 10
-b = 5
+    a = 10
+    b = 5
 
-result_add = add(a, b)
-result_subtract = subtract(a, b)
-result_multiply = multiply(a, b)
-result_divide = divide(a, b)
-
-print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
