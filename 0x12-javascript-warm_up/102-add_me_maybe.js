e3dd6e47eab647163890959b6bce3498922438c7
Terry-Gumani <terrencegumani0@gmail.com>
2023-09-13 06:09:44 -0700

    Semistandard fix - add semicolon

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,4 +1,13 @@
 #!/usr/bin/node
-exports.addMeMaybe = function (number, theFunction) {
-  theFunction(++number);
-};
+function incrementAndCall(number, theFunction) {
+  let incrementedNumber = number + 1;
+  theFunction(incrementedNumber);
+}
+
+// Example usage:
+function exampleFunction(result) {
+  console.log(`The result is ${result}`);
+}
+
+incrementAndCall(5, exampleFunction); // Output: The result is 6
+
