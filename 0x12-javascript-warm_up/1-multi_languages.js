fae7352c4d7aee9198899371bcbdceb58a280bb2
Terry-Gumani <terrencegumani0@gmail.com>
2023-11-13 01:02:04 -0800

    Upload task 0: script that prints 'Javascript is amazing'

--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const cFun = 'C is fun';
-const pyCool = 'Python is cool';
-const jAmaze = 'JavaScript is amazing';
-
-console.log(cFun);
-console.log(pyCool);
-console.log(jAmaze);
